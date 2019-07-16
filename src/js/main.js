27ba27e86d7ffa673606a77aade80ef6c9887ef6
Sylwester Gladki <sylwester.gladki@gmail.com>
2019-07-16 19:12:39 +0200

    feat: basic js

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,35 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+
+
+const addGlass = document.querySelector('.button--add');
+const deleteGlass = document.querySelector('.button--delete');
+let numberGlass =  document.querySelector('.number--js');
+
+
+numberGlass.innerHTML = 0;
+
+
+
+addGlass.addEventListener("click", (e) => {
+  e.preventDefault();
+  numberGlass.innerHTML++;
+  localStorage.setItem('water', numberGlass.innerHTML);
+});
+
+if ( localStorage.getItem('water') > 0){
+
+  deleteGlass.addEventListener("click", (e) => {
+  e.preventDefault();
+  numberGlass.innerHTML--;
+  localStorage.setItem('water', numberGlass.innerHTML);
+  });
+};
+  
+
+
+
+
 
 
